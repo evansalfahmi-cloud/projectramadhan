fff33c0a7c4c474d7c5efd3975338e5cfec0743e
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2025-03-15 15:07:59 +0700

    add header, navbar components, fix blog, about, contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,14 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <title>contact</title>
 </head>
 <body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/contact">Contact</a>
-    <H1>halaman contact</H1>
+    <div class="min-h-full">
+        <x-navbar></x-navbar>
+        <x-header>Contact</x-header>
+    
+    
+        <main> 
+          <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <!-- Your content -->
+            <p>welcome to contact</p>
+          </div>
+        </main>
+      </div>
     
 </body>
 </html>
