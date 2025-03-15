c6c753df9aa7297ebcb16768b9180b8a08b05afd
evansalfahmi-cloud <evansalfahmi.org@gmail.com>
2025-03-15 16:44:21 +0700

    perbaikan tittle header , web.php

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <title>Test</title>
+    <title>test</title>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 <body class="h-full"> 
